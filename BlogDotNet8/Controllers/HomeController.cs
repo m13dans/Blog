2d2daf6815baf758fa2211341eccb3ddd4c0535c
Danur Arya <danuraryapratama@gmail.com>
2024-01-12 14:08:48 +0700

    Blog Project Created using ASP.NET Core Razor

---
+++ BlogDotNet8/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDotNet8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+    }
+}
