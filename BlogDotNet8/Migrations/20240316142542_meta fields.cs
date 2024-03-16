0b7446590271171297f00de4dbe9a65a5c130459
Danur Arya <danuraryapratama@gmail.com>
2024-03-16 21:38:40 +0700

    create meta field in post post class

---
+++ BlogDotNet8/Migrations/20240316142542_meta fields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogDotNet8.Migrations
+{
+    /// <inheritdoc />
+    public partial class metafields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Posts");
+        }
+    }
+}
--- BlogDotNet8/Models/Post.cs
+++ BlogDotNet8/Models/Post.cs
@@ -6,9 +6,14 @@ namespace BlogDotNet8.Models
     {
         [Key]
         public int Id { get; set; }
+
         public string Title { get; set; } = "";
         public string Body { get; set; } = "";
         public string Image {get; set;} = "";
+
+        public string Description { get; set; } ="";
+        public string Tags { get; set; } ="";
+        public string Category { get; set; } ="";
         public DateTime Created { get; set; } = DateTime.Now;
     }
 }
